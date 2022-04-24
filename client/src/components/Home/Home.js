d37a06d0abdb4a507269b91102b5091df8e5b25a
Abantika Saha <abantika.saha18@gmail.com>
2022-04-25 02:58:26 +0530

    added calendar

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,9 +5,9 @@ const Home = ({ open }) => {
   const classes = useStyles();
   return (
     <div className={open ? classes.root : null}>
-      <h1>Home..</h1>
+      <h1>Schedule</h1>
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=family03935848505130645796%40group.calendar.google.com&ctz=Asia%2FKolkata"
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FKolkata&showTitle=0&src=c29oaW5pMjAxOUBnbWFpbC5jb20&src=Y2xhc3Nyb29tMTAzNjg0NDM4NTYwOTEyMzU5ODEyQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%237627bb"
         width="100%"
         height="500"
       ></iframe>
